222e6ab56fd9803c967582f7aa313fc867e61427
chc8909 <chc8909@gmailcom>
2020-12-13 19:29:46 +0900

    Feature. step3 각 면 회전시키는 함수 구현
    하드코딩으로...

--- step3.js
+++ step3.js
@@ -100,3 +100,163 @@ function counterClockRotate(side) {
     }
     return temp
 }
+
+
+//앞면 시계방향
+function frontClockWise() {
+    let oStorage = makeColumn(o, 2)
+    let wStorage = w[2]
+    let rStorage = makeColumn(r, 0)
+    let yStorage = y[0]
+
+    w[2] = oStorage
+    addToColumn0(wStorage, r)
+    y[0] = rStorage
+    addToColumn2(yStorage, o)
+    g = clockRotate(g)
+}
+
+//앞면 반시계방향
+function frontInverted() {
+    let oStorage = makeColumn(o, 2)
+    let wStorage = w[2]
+    let rStorage = makeColumn(r, 0)
+    let yStorage = y[0]
+
+    w[2] = rStorage
+    addToColumn0(yStorage, r)
+    o[0] = oStorage
+    addToColumn2(wStorage, o)
+    g = counterClockRotate(g)
+}
+
+function rightClockWise() {
+    let wStorage = makeColumn(w, 2)
+    let gStorage = makeColumn(g, 2)
+    let yStorage = makeColumn(y, 2)
+    let bStorage = makeColumn(b, 0)
+
+    addToColumn2(gStorage, w)
+    addToColumn2(yStorage, g)
+    addToColumn2(bStorage, y)
+    addToColumn0(wStorage, b)
+    r = clockRotate(r)
+}
+
+function rightInverted() {
+    let wStorage = makeColumn(w, 2)
+    let gStorage = makeColumn(g, 2)
+    let yStorage = makeColumn(y, 2)
+    let bStorage = makeColumn(b, 0)
+
+    addToColumn2(bStorage, w)
+    addToColumn2(wStorage, g)
+    addToColumn2(gStorage, y)
+    addToColumn0(yStorage, b)
+    r = counterClockRotate(r)
+}
+
+function upClockWise() {
+    let oStorage = o[0]
+    let gStorage = g[0]
+    let rStorage = r[0]
+    let bStorage = b[0]
+
+    o[0] = gStorage
+    g[0] = rStorage
+    r[0] = bStorage
+    b[0] = oStorage
+    w = clockRotate(w)
+}
+
+function upInverted() {
+    let oStorage = o[0]
+    let gStorage = g[0]
+    let rStorage = r[0]
+    let bStorage = b[0]
+
+    o[0] = bStorage
+    g[0] = oStorage
+    r[0] = gStorage
+    b[0] = rStorage
+    w = counterClockRotate(w)
+}
+
+function bottomClockWise() {
+    let oStorage = makeColumn(o, 0)
+    let wStorage = w[0]
+    let rStorage = makeColumn(r, 2)
+    let yStorage = y[2]
+
+    addToColumn0(wStorage, o)
+    w[0] = rStorage
+    addToColumn2(yStorage, r)
+    y[2] = oStorage
+    b = clockRotate(b)
+}
+
+function bottomInverted() {
+    let oStorage = makeColumn(o, 0)
+    let wStorage = w[0]
+    let rStorage = makeColumn(r, 2)
+    let yStorage = y[2]
+
+    addToColumn0(yStorage, o)
+    w[0] = oStorage
+    addToColumn2(wStorage, r)
+    y[2] = rStorage
+    b = counterClockRotate(b)
+}
+
+function leftClockWise() {
+    let wStorage = makeColumn(w, 0)
+    let gStorage = makeColumn(g, 0)
+    let yStorage = makeColumn(y, 0)
+    let bStorage = makeColumn(b, 2)
+
+    addToColumn0(bStorage, w)
+    addToColumn0(wStorage, g)
+    addToColumn0(gStorage, y)
+    addToColumn2(yStorage, b)
+    o = clockRotate(o)
+}
+
+function leftInverted() {
+    let wStorage = makeColumn(w, 0)
+    let gStorage = makeColumn(g, 0)
+    let yStorage = makeColumn(y, 0)
+    let bStorage = makeColumn(b, 2)
+
+    addToColumn0(gStorage, w)
+    addToColumn0(yStorage, g)
+    addToColumn0(bStorage, y)
+    addToColumn2(wStorage, b)
+    o = counterClockRotate(o)
+}
+
+function downClockWise() {
+    let oStorage = o[2]
+    let gStorage = g[2]
+    let rStorage = r[2]
+    let bStorage = b[2]
+
+    o[2] = bStorage
+    g[2] = oStorage
+    r[2] = gStorage
+    b[2] = rStorage
+    y = clockRotate(y)
+}
+
+function downInverted() {
+    let oStorage = o[2]
+    let gStorage = g[2]
+    let rStorage = r[2]
+    let bStorage = b[2]
+
+    o[2] = gStorage
+    g[2] = rStorage
+    r[2] = bStorage
+    b[2] = oStorage
+    y = counterClockRotate(y)
+}
+////////////////////////////////////////////////////////////////////////////
